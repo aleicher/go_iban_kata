9adc07b94e2518df40e74f64e618c8b3881e27e2
Andreas Leicher <andreas.leicher@mapcase.de>
2014-03-26 21:41:10 +0100

    add new tests for validation 2

--- iban/iban_test.go
+++ iban/iban_test.go
@@ -14,17 +14,30 @@ var _ = Describe("IbanValidator", func() {
 	})
 	Describe("Validation 1: maximum length", func() {
 		shortIban := "123456"
-		longIban := "1234567890123456789012345678901234"
-		tooLongIban := "12345678901234567890123456789012345"
+		tooLongIban := "DE345678901234567890123456789012345"
 
 		It("does provide a function to check if an IBAN is valid", func() {
 			Expect(iban.IsValid(shortIban)).To(Equal(true))
 		})
 		It("does return true for a IBAN with 34 characters", func() {
-			Expect(iban.IsValid(longIban)).To(Equal(true))
+			Expect(iban.IsValid(validIbanFixture)).To(Equal(true))
 		})
 		It("does return false for an IBAN with more than 34 characters", func() {
 			Expect(iban.IsValid(tooLongIban)).To(Equal(false))
 		})
+	})
+	Describe("Validation 2: first two positions are characters", func() {
+		noCharIban := "1234567890123456789012345678901234"
+		oneCharIban := "X234567890123456789012345678901234"
+		It("does return false for an IBAN which starts with numbers", func() {
+			Expect(iban.IsValid(noCharIban)).To(Equal(false))
+		})
+		It("does return false for an IBAN which starts with one character", func() {
+			Expect(iban.IsValid(oneCharIban)).To(Equal(false))
+		})
+		It("does return true for the valid IBAN", func() {
+			Expect(iban.IsValid(validIbanFixture)).To(Equal(true))
+		})
+
 	})
 })
