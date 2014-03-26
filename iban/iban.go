91851beed20d2d9d83aaddf9269e2dc227f1e4f0
Andreas Leicher <andreas.leicher@mapcase.de>
2014-03-26 22:23:17 +0100

    refactor to use unicode package

--- iban/iban.go
+++ iban/iban.go
@@ -1,31 +1,18 @@
 package iban
 
+import (
+	"unicode"
+)
+
 type IbanValidator struct {
 }
 
-// The following few functions are kind of unnecessary.
-// because I don't know how to do it for a single byte in Go
-func IsLower(c uint8) bool {
-  return (('a' <= c) && (c <= 'z'))
-}
-
-func IsUpper(c uint8) bool {
-  return (('A' <= c) && (c <= 'Z'))
-}
-
-func IsAlpha(c uint8) bool {
-  return IsUpper(c) || IsLower(c)
-}
-
-func IsDigit(c uint8) bool {
-  return (('0' <= c) && (c <= '9'))
-}
-
 func (iv *IbanValidator) IsValid(s string) (ret bool) {
-  form_test_1 := (len(s) <= 34)
-  form_test_2 := IsAlpha(s[0]) && IsAlpha(s[1])
-  form_test_3 := IsDigit(s[2]) && IsDigit(s[3])
+	runes := []rune(s)
+	form_test_1 := (len(s) <= 34)
+	form_test_2 := unicode.IsLetter(runes[0]) && unicode.IsLetter(runes[1])
+	form_test_3 := unicode.IsDigit(runes[2]) && unicode.IsDigit(runes[3])
 
-  form_test_full := form_test_1 && form_test_2 && form_test_3
-  return form_test_full
+	form_test_full := form_test_1 && form_test_2 && form_test_3
+	return form_test_full
 }
