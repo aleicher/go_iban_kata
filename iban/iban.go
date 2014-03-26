2e83b332bee17b5b4ab1dbfa7a0f4f317b0665d8
Sandeep Sadanandan <sandeep.sadanandan@mapcase.de>
2014-03-26 22:08:09 +0100

    adding function to check digits, and using it for positions 3 and 4

--- iban/iban.go
+++ iban/iban.go
@@ -17,10 +17,15 @@ func IsAlpha(c uint8) bool {
   return IsUpper(c) || IsLower(c)
 }
 
+func IsDigit(c uint8) bool {
+  return (('0' <= c) && (c <= '9'))
+}
+
 func (iv *IbanValidator) IsValid(s string) (ret bool) {
   form_test_1 := (len(s) <= 34)
   form_test_2 := IsAlpha(s[0]) && IsAlpha(s[1])
+  form_test_3 := IsDigit(s[2]) && IsDigit(s[3])
 
-  form_test_full := form_test_1 && form_test_2
+  form_test_full := form_test_1 && form_test_2 && form_test_3
   return form_test_full
 }
