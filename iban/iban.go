ceedcf44c1bdba95e4a40ced4be38ec93ba8ccf7
Sandeep Sadanandan <sandeep.sadanandan@mapcase.de>
2014-03-27 18:57:35 +0100

    checking for the validity of characters 5 -- 34

--- iban/iban.go
+++ iban/iban.go
@@ -1,18 +1,25 @@
 package iban
 
 import (
-	"unicode"
+  "unicode"
 )
 
 type IbanValidator struct {
 }
 
 func (iv *IbanValidator) IsValid(s string) (ret bool) {
-	runes := []rune(s)
-	form_test_1 := (len(s) <= 34)
-	form_test_2 := unicode.IsLetter(runes[0]) && unicode.IsLetter(runes[1])
-	form_test_3 := unicode.IsDigit(runes[2]) && unicode.IsDigit(runes[3])
+  runes := []rune(s)
+  form_test_1 := (len(s) <= 34)
+  form_test_2 := unicode.IsLetter(runes[0]) && unicode.IsLetter(runes[1])
+  form_test_3 := unicode.IsDigit(runes[2]) && unicode.IsDigit(runes[3])
+  form_test_4 := true
+  for _, x := range runes[4:] {
+    if unicode.IsDigit(x) == false {
+      form_test_4 = false
+      break
+    }
+  }
 
-	form_test_full := form_test_1 && form_test_2 && form_test_3
-	return form_test_full
+  form_test_full := form_test_1 && form_test_2 && form_test_3 && form_test_4
+  return form_test_full
 }
